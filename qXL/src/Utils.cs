2b71dc7d7e2d0d1cd830940b7e46dcd27e91af56
Marc Recht <m.recht@devnet.de>
2015-08-28 15:16:27 +0200

    update copyright

--- qXL/src/Utils.cs
+++ qXL/src/Utils.cs
@@ -1,5 +1,5 @@
 ﻿// 
-// Copyright (c) 2011-2014 Exxeleron GmbH
+// Copyright (c) 2011-2015 Exxeleron GmbH
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
